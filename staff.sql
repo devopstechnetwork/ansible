864dcac9f44e9826a234ef4618f308aefb2f0c9e
sk3ma <52137438+sk3ma@users.noreply.github.com>
2019-10-22 22:05:53 +0200

    Delete staff.sql

--- staff.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE staff(
-id INT not null AUTO_INCREMENT PRIMARY KEY,
-fname VARCHAR(20) null,
-lname VARCHAR(20) null,
-age INT not null
-department VARCHAR(20) null,
-email VARCHAR(20) null,
-)ENGINE=innodbs
